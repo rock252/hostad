09ab115ad4f819563f1bcf78f1e9fbd41052c045
shivam <rockbox252@gmail.com>
2018-06-03 03:37:10 +0530

    update session

--- accounts/views.py
+++ accounts/views.py
@@ -4,18 +4,20 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from . forms import *
 def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(username=cd['username'],password=cd['password'])
-            if user is not None:
-                login(request, user)
-                
-                return redirect('customer_portal1')
-            else:
-                return redirect('first')
+    if request.user.is_authenticated():
+        return redirect('customer_portal1')
     else:
-        form = LoginForm()
-    return render(request, 'accounts/login.html', {'form': form})
+        if request.method == 'POST':
+            form = LoginForm(request.POST)
+            if form.is_valid():
+                cd = form.cleaned_data
+                user = authenticate(username=cd['username'],password=cd['password'])
+                if user is not None:
+                    login(request, user)
+                    return redirect('customer_portal1')
+                else:
+                    return redirect('first')
+        else:
+            form = LoginForm()
+        return render(request, 'accounts/login.html', {'form': form})
 
