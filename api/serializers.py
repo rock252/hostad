2a1e949db3e83eece431de68983fd8082e14bb5d
shivam <rockbox252@gmail.com>
2018-12-17 04:23:51 +0530

    17/12

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from api.models import Information , Sendtotab
+from api.models import *
 
 class postSerializer(serializers.ModelSerializer):
    class Meta:
